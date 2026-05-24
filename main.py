e5cdfce13c139d82d5abcb3b98dfea488660df85
Aashutosh Chaudhary <newashutosh.team7@gmail.com>
2026-05-25 00:53:07 +0545

    Refactor main.py and add slicing.py for array manipulation examples

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 import numpy as np
 
-print(np.__version__)
+# print(np.__version__) # checking numpy version
+
+
+# very basics
+# array = np.array([1,2,3,4])
+
+# array = array * 2
+
+# print(array)
+
+# array = np.array(['A', 'B', 'C']) # 1D array
+array = np.array([['A', 'B', 'C'],
+                 ['D', 'E', 'F'],
+                 ['G', 'H', 'I']]) # 2D array
+print(array.ndim)
+print(array.shape)
+
+
+# Accessing elements through Multidimensional indexing
+print(array[0, 1]) # prints 'B'
